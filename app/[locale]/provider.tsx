799df3ca84762cae9aeb798768943c57ff6073be
AmrAbdelhady1 <37872476+AmrAbdelhady1@users.noreply.github.com>
2025-07-17 17:08:47 +0300

    Update provider.tsx

--- app/[locale]/provider.tsx
+++ app/[locale]/provider.tsx
@@ -6,16 +6,20 @@ import ReduxProvider from "./reduxProvider";
 const Provider = async ({ child, lang }: any) => {
   const token = await getCookie("auth-token");
   const countryId = await getCookie("country_id");
-  const countries = await useCountries(lang);
-  const categories = await useCategories(lang);
-  const username = await useProfile(token);
+  // const countries = await useCountries(lang);
+  // const categories = await useCategories(lang);
+  // const username = await useProfile(token);
+
+  // console.log({username});
+  // console.log({countries});
+  // console.log({categories});
 
   return (
       <ReduxProvider
         child={child}
-        countries={countries}
-        categories={categories}
-        username={username}
+        // countries={countries}
+        // categories={categories}
+        // username={username}
         lang={lang}
         countryId={countryId}
       />
